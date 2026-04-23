ff459b8f9b6fefdb5be55fc11282d7787cac0ba5
Riju Bose <riju7437@gmail.com>
2026-04-23 22:02:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = "todo-app-v1";
+
+const filesToCache = [
+  "index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
